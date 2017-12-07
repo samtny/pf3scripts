1b72f0005756efddc1c2eb9d57ff6528624d9664
Sam Thompson <sthompson@digitalpulp.com>
2017-12-07 11:24:47 -0500

    move backup

--- samtny_pf3_backup.sh
+++ samtny_pf3_backup.sh
@@ -8,7 +8,7 @@ DB_NAME=samtny_pf3
 
 MYSQL_DEFAULTS_FILE=${HOME_DIR}/.${DB_NAME}.my.cnf
 
-BACKUP_DIR=${HOME_DIR}/backup
+BACKUP_DIR=${HOME_DIR}/pinfinder_backup
 BACKUP_FILENAME=${DB_NAME}.sql
 BACKUP_FILE=${BACKUP_DIR}/${BACKUP_FILENAME}
 
@@ -21,4 +21,3 @@ mkdir -p ${BACKUP_DIR}
 savelog -n -c ${BACKUP_DAYS} ${BACKUP_FILE}
 
 exit 0
-
