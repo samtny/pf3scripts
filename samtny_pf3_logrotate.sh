9492894c3efa67606a4393421bcecc42c076b843
Sam Thompson <sthompson@digitalpulp.com>
2017-12-12 14:40:37 -0500

    logrotate

---
+++ samtny_pf3_logrotate.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+HOME_DIR=/usr/home/samtny
+
+LOG_DIR=${HOME_DIR}/pinfinder_logs
+LOG_ROTATE_DAYS=30
+
+LOG_FILENAME=scrape.log
+LOG_FILE=${LOG_DIR}/${LOG_FILENAME}
+
+savelog -n -c ${LOG_ROTATE_DAYS} ${LOG_FILE}
+
+LOG_FILENAME=backup.log
+LOG_FILE=${LOG_DIR}/${LOG_FILENAME}
+
+savelog -n -c ${LOG_ROTATE_DAYS} ${LOG_FILE}
+
+exit 0
