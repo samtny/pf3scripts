a11438b88c022f05d9c1454ae718acfd191de9a6
Sam Thompson <sthompson@digitalpulp.com>
2017-12-12 16:19:09 -0500

    fix home dir

--- setup.sh
+++ setup.sh
@@ -7,17 +7,17 @@ HOME_DIR=/usr/home/samtny
 mkdir -p ${HOME_DIR}/pinfinder_logs/pf3
 mkdir -p ${HOME_DIR}/pinfinder_backup
 
-croncmd="/usr/home/samtny/pf3scripts/backup.sh >> /usr/home/samtny/pinfinder_logs/pf3/backup.log 2>&1"
+croncmd="${HOME_DIR}/pf3scripts/backup.sh >> /usr/home/samtny/pinfinder_logs/pf3/backup.log 2>&1"
 cronjob="19 12 * * * $croncmd"
 
 ( crontab -l | grep -v -F "$croncmd" ; echo "$cronjob" ) | crontab -
 
-croncmd="/usr/home/samtny/pf3scripts/scrape.sh >> /usr/home/samtny/pinfinder_logs/pf3/scrape.log 2>&1"
+croncmd="${HOME_DIR}/pf3scripts/scrape.sh >> /usr/home/samtny/pinfinder_logs/pf3/scrape.log 2>&1"
 cronjob="19 2 * * *  $croncmd"
 
 ( crontab -l | grep -v -F "$croncmd" ; echo "$cronjob" ) | crontab -
 
-croncmd="/usr/home/samtny/pf3scripts/logrotate.sh"
+croncmd="${HOME_DIR}/pf3scripts/logrotate.sh"
 cronjob="19 3 * * *  $croncmd"
 
 ( crontab -l | grep -v -F "$croncmd" ; echo "$cronjob" ) | crontab -
