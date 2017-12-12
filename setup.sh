431b4e71b4b884c407ad63be7fa29d384755e8ea
Sam Thompson <sthompson@digitalpulp.com>
2017-12-12 16:19:51 -0500

    remove space

--- setup.sh
+++ setup.sh
@@ -13,12 +13,12 @@ cronjob="19 12 * * * $croncmd"
 ( crontab -l | grep -v -F "$croncmd" ; echo "$cronjob" ) | crontab -
 
 croncmd="${HOME_DIR}/pf3scripts/scrape.sh >> /usr/home/samtny/pinfinder_logs/pf3/scrape.log 2>&1"
-cronjob="19 2 * * *  $croncmd"
+cronjob="19 2 * * * $croncmd"
 
 ( crontab -l | grep -v -F "$croncmd" ; echo "$cronjob" ) | crontab -
 
 croncmd="${HOME_DIR}/pf3scripts/logrotate.sh"
-cronjob="19 3 * * *  $croncmd"
+cronjob="19 3 * * * $croncmd"
 
 ( crontab -l | grep -v -F "$croncmd" ; echo "$cronjob" ) | crontab -
 
