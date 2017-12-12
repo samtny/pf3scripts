05dba938e121745428ce6c014af6bf72f04e5bc4
Sam Thompson <sthompson@digitalpulp.com>
2017-12-12 16:18:25 -0500

    setup cron

--- setup.sh
+++ setup.sh
@@ -7,4 +7,19 @@ HOME_DIR=/usr/home/samtny
 mkdir -p ${HOME_DIR}/pinfinder_logs/pf3
 mkdir -p ${HOME_DIR}/pinfinder_backup
 
+croncmd="/usr/home/samtny/pf3scripts/backup.sh >> /usr/home/samtny/pinfinder_logs/pf3/backup.log 2>&1"
+cronjob="19 12 * * * $croncmd"
+
+( crontab -l | grep -v -F "$croncmd" ; echo "$cronjob" ) | crontab -
+
+croncmd="/usr/home/samtny/pf3scripts/scrape.sh >> /usr/home/samtny/pinfinder_logs/pf3/scrape.log 2>&1"
+cronjob="19 2 * * *  $croncmd"
+
+( crontab -l | grep -v -F "$croncmd" ; echo "$cronjob" ) | crontab -
+
+croncmd="/usr/home/samtny/pf3scripts/logrotate.sh"
+cronjob="19 3 * * *  $croncmd"
+
+( crontab -l | grep -v -F "$croncmd" ; echo "$cronjob" ) | crontab -
+
 exit 0
